1e7cb911b5990ac0858fdd0eddab30ff8601c309
Newaz579 <newazsharif329@gmail.com>
2022-06-14 19:55:01 +0600

    styles added

---
+++ js/album-photos-v-9.js
@@ -0,0 +1,16 @@
+function loadAlbums() {
+    fetch('https://jsonplaceholder.typicode.com/albums')
+        .then(res => res.json())
+        .then(data => displayAlbums(data))
+}
+loadAlbums();
+function displayAlbums(albums) {
+    console.log(albums);
+    const albumsTitle = document.getElementById('albums');
+    for (const album of albums) {
+        const p = document.createElement('p');
+        p.style.textAlign = 'center';
+        p.innerText = album.title;
+        albumsTitle.appendChild(p);
+    }
+}
